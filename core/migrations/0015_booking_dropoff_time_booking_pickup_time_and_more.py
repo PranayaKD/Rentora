060c89e416fec87dd04103182bef929f52502999
Agro-Tech <dashpranaya786@gmail.com>
2026-03-27 13:53:25 +0530

    Fixed some error

--- core/migrations/0015_booking_dropoff_time_booking_pickup_time_and_more.py
+++ core/migrations/0015_booking_dropoff_time_booking_pickup_time_and_more.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0014_booking_booking_reference'),
+        ('core', '0013_booking_abandoned_reminder_sent'),
     ]
 
     operations = [
